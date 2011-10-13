0c0318ec30e676e64254e75a7adae995c33b03b1
wkoziej@gmail.com <wkoziej@gmail.com@fd38e74b-d074-9a3f-43af-de8dd200d479>
2011-10-13 20:36:18 +0000

    Working browser

--- trunk/browser/mainwindow.cpp
+++ trunk/browser/mainwindow.cpp
@@ -1,37 +1,134 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <qt4/QtSql/QSqlDatabase>
-#include <qt4/QtSql/QSqlTableModel>
+#include <QtSql/qsqlerror.h>
+#include <QtSql/qsqlrecord.h>
+
+LoggerPtr MainWindow::logger(Logger::getLogger("browser"));
+
 MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+	QMainWindow(parent), ui(new Ui::MainWindow) {
+	ui->setupUi(this);
+
+	QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+	if (db.isValid())
+		db.setDatabaseName("../simulator/project-world.db");
+	if (!db.isValid() || !db.open()) {
+		// Give error message, e.g., using QMessageBox
+		// QMessageBox::information("Error", "Cannot open database");
+		qDebug("Cannot open database");
+		return;
+	}
 
 
-    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-
-    if (db.isValid())
-        db.setDatabaseName("project-world.db");
-    if (!db.isValid() || !db.open()) {
-        // Give error message, e.g., using QMessageBox
-        // QMessageBox::information("Error", "Cannot open database");
-        qDebug("Cannot open database");
-        return;
-    }
 
 
-    QSqlTableModel *model = new QSqlTableModel();
-    model->setTable("v_worlds_runs");
-    model->select();
-    model->setEditStrategy(QSqlTableModel::OnManualSubmit);
-    //model->setHeaderData(0, Qt::Horizontal, tr("Name"));
-    //model->setHeaderData(1, Qt::Horizontal, tr("Salary"));
 
-    ui->worldRunsTableView->setModel(model);
+	setUpTable(ui->worldRunsTableView, "v_worlds_runs", worldRunsModel);
+	setUpTable(ui->worldSnapshotTableView, "v_worlds_snapshots",
+			worldSnapshotModel);
+	setUpTable(ui->fieldSnapshotTableView, "v_fields_snapshots",
+			fieldSnapshotModel);
+
+	setUpTable(ui->populationCreaturesTableView, "v_creatures_snapshots",
+			populationCreatureSnapshotModel);
+
+	setUpTable(ui->fieldPopulationsTableView, "v_populations_snapshots",
+			fieldPopulationSnapshotModel);
+	setUpTable(ui->fieldProductsTableView, "v_fields_products_snapshots",
+			fieldProductsSnapshotModel);
+	setUpTable(ui->fieldResourcesTableView, "v_fields_resources_snapshots",
+			fieldResourcesSnapshotModel);
+
+
+
+
+	connect(
+			ui->worldRunsTableView->selectionModel(),
+			SIGNAL(currentRowChanged ( const QModelIndex & , const QModelIndex & )),
+			this, SLOT(changeWorldRun(const QModelIndex &)));
+	connect(
+			ui->worldSnapshotTableView->selectionModel(),
+			SIGNAL(currentRowChanged ( const QModelIndex & , const QModelIndex & )),
+			this, SLOT(changeWorldSnapshot(const QModelIndex &)));
+
+	connect(
+			ui->fieldSnapshotTableView->selectionModel(),
+			SIGNAL(currentRowChanged ( const QModelIndex & , const QModelIndex & )),
+			this, SLOT(changeFieldSnapshot(const QModelIndex &)));
+
+	connect(
+			ui->fieldPopulationsTableView->selectionModel(),
+			SIGNAL(currentRowChanged ( const QModelIndex & , const QModelIndex & )),
+			this, SLOT(changePopulationSnapshot(const QModelIndex &)));
+
+
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+void MainWindow::setUpTable(QTableView *tableView, QString sqlTableName,
+		QSqlTableModel *&model) {
+	model = new QSqlTableModel();
+	model->setTable(sqlTableName);
+	model->select();
+	model->setEditStrategy(QSqlTableModel::OnManualSubmit);
+	tableView->setModel(model);
+	tableView->hideColumn(0);
+	tableView->hideColumn(1);
+	tableView->resizeColumnsToContents();
+	if (!model->select()) {
+		LOG4CXX_ERROR (logger, "Error selecting rows: " << model->lastError().text().toStdString());
+	}
+}
+
+void MainWindow::changeWorldRun(const QModelIndex &index) {
+	changeMasterId(worldRunsModel, index, worldSnapshotModel, "world_run_id",
+			ui->worldSnapshotTableView);
+}
+
+void MainWindow::changeWorldSnapshot(const QModelIndex &index) {
+	changeMasterId(worldSnapshotModel, index, fieldSnapshotModel,
+			"world_snapshot_id", ui->fieldSnapshotTableView);
+}
+
+void MainWindow::changeFieldSnapshot(const QModelIndex &index) {
+	LOG4CXX_DEBUG(logger, "changeFieldSnapshot " << " index.row = " << index.row());
+
+	changeMasterId(fieldSnapshotModel, index, fieldPopulationSnapshotModel,
+			"field_snapshot_id", ui->fieldPopulationsTableView);
+	changeMasterId(fieldSnapshotModel, index, fieldResourcesSnapshotModel,
+			"field_snapshot_id", ui->fieldResourcesTableView);
+	changeMasterId(fieldSnapshotModel, index, fieldProductsSnapshotModel,
+			"field_snapshot_id", ui->fieldProductsTableView);
+}
+
+void MainWindow::changePopulationSnapshot(const QModelIndex &index) {
+	LOG4CXX_DEBUG(logger, "changePopulationSnapshot " << " index.row = " << index.row());
+	changeMasterId(fieldPopulationSnapshotModel, index,
+			populationCreatureSnapshotModel, "population_snapshot_id",
+			ui->populationCreaturesTableView);
+}
+
+void MainWindow::changeMasterId(QSqlTableModel *master,
+		const QModelIndex &index, QSqlTableModel *detail,
+		QString masterColumnName, QTableView *datailTableView) {
+	LOG4CXX_DEBUG(logger, "changeMasterId, detail name << " << datailTableView->objectName().toStdString()
+			<< " index.row = " << index.row());
+	if (index.row() >= 0) {
+		QModelIndex pk = master->index(index.row(), 0);
+		QString detailCondition = masterColumnName + " = "
+				+ pk.data().toString();
+		LOG4CXX_DEBUG(logger, "changeMasterId, detailCondition = " << detailCondition.toStdString() );
+		detail->setFilter(detailCondition);
+		datailTableView->selectRow(0);
+	} else if (index.row() == 0) {
+		detail->setFilter(QString());
+	} else {
+		return;
+	}
+}
+
+MainWindow::~MainWindow() {
+	delete ui;
+	QSqlDatabase db = QSqlDatabase::database();
+	db.close();
 }
