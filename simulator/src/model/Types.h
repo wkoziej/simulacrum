21437569fe0a4187219e046f4ac1f16b62524196
wkoziej@gmail.com <wkoziej@gmail.com@fd38e74b-d074-9a3f-43af-de8dd200d479>
2011-10-16 21:35:18 +0000

    Dont compile yet..

---
+++ simulator/src/model/Types.h
@@ -0,0 +1,17 @@
+/*
+ * Types.h
+ *
+ *  Created on: 16-10-2011
+ *      Author: wojtas
+ */
+
+#ifndef TYPES_H_
+#define TYPES_H_
+
+typedef std::pair<std::wstring, CreaturesPopulation *> NamedPopulation;
+typedef std::map<std::wstring, CreaturesPopulation *> PopulationsMap;
+typedef std::vector<float> FloatVector;
+
+
+
+#endif /* TYPES_H_ */
